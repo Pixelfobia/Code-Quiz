40925bd8ca4088634d65704e0211f18337bc45e7
Sergio Utrera <sergiactive@gmail.com>
2023-01-17 19:11:20 +0000

    Added questions.js

---
+++ questions.js
@@ -0,0 +1,33 @@
+//Array of Objects containing 5 questions and answer choices
+let questions = [
+	{
+		title: "Commonly used data types DO NOT include:",
+		choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+		answer: "3. alerts",
+	},
+
+	{
+		title: "The condition in an if/else statement is enclosed within _____.",
+		choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+		answer: "2. curly brackets"
+	},
+
+	{
+		title: "Arrays in JavaScript can be used to store ____.",
+		choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+		answer: "4. all of the above"
+
+	},
+
+	{
+		title: "String values must be enclosed within _____ when being assigned to variables",
+		choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+		answer: "3. quotes"
+	},
+
+	{
+		title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+		choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
+		answer: "4. console log"
+	},
+];
